745ff83d23277f217ccd61c3c87096f286655d87
niklashansson <niklas@bloomweb.co>
2023-09-19 21:06:03 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { demoModal } from '$utils/demoModal';
 import { fetchItems } from '$utils/fetchItems';
 import { navbar } from '$utils/navbar';
 import { resources } from '$utils/resources';
@@ -5,10 +6,11 @@ import { globalSwiper } from '$utils/swiper';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
+  demoModal();
   fetchItems();
   globalSwiper();
   navbar();
   resources();
 });
 
-/* <script defer src="https://cdn.jsdelivr.net/gh/niklashansson/verified.eu@9d6e1a0be9de38446d811abd556044f93b92738f/dist/index.min.js"></script> */
+/* <script defer src="https://cdn.jsdelivr.net/gh/niklashansson/verified.eu@1eaad8a35aa8f93e63fe48cf0aa3ba8936ebc1b9/dist/index.min.js"></script> */
---
+++ src/utils/demoModal.ts
@@ -0,0 +1,23 @@
+export const demoModal = function () {
+  const buttons = document.querySelectorAll('a[href="#book-demo"]');
+  const modal = document.querySelector('[verified-demo-element="modal"]');
+  console.log(buttons);
+  if (!buttons || !modal) return;
+
+  const closeButtons = modal.querySelectorAll('[verified-demo-element="close-button"]');
+  if (!closeButtons) return;
+
+  buttons.forEach((btn) => {
+    btn.setAttribute('href', '#/');
+
+    btn.addEventListener('click', (e) => {
+      modal.classList.add('is-active');
+    });
+  });
+
+  closeButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      modal.classList.remove('is-active');
+    });
+  });
+};
